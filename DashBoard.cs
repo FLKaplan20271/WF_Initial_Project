0e551d7198afe1cfffd5aa02d6b59604a5e1e98e
FLKaplan20271 <frank.kaplan@gmail.com>
2020-11-15 04:30:16 -0700

    whenever you have an issue with unhandled exception with not providing primary key - you either don't have the primary key added to the query, or, what you want to have as a primary key, is not set as one.  My issue was that primary key was not added to query.

--- DashBoard.cs
+++ DashBoard.cs
@@ -7,14 +7,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace WF_Initial_Project
 {
+
     public partial class DashBoard : Form
     {
+        SqlDataAdapter sda;
+        SqlCommandBuilder sbc;
+        DataTable dt;
+
         public DashBoard()
         {
             InitializeComponent();
+        }
+
+        private void DashBoard_Load(object sender, EventArgs e)
+        {
+            //// TODO: This line of code loads data into the 'personData.tblPersonData' table. You can move, or remove it, as needed.
+            //this.dtaTblPerson.Fill(this.dsPersonData.tblPersonData);
+
+        }
+
+        private void btnShowData_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=franksdb;Integrated security=True;");
+            sda = new SqlDataAdapter(@"select id,fname,lname,email,gender,address, city,state,postal,phonenumber,yearlysalary,unitname,ip_address from tblPersonData",con);
+            dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btnUpdateRecords_Click(object sender, EventArgs e)
+        {
+            sbc = new SqlCommandBuilder(sda);
+            sda.Update(dt);
         }
     }
 }
